513def335e4bd2beaa45e7c8f148e54585b7fc72
Takayuki Sei <takayuki_sei@voyagegroup.com>
2013-09-03 00:43:04 +0900

    Initial Commit

---
+++ MojaPiyoTataki/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  MojaPiyoTataki
+//
+//  Created by Sei Takayuki on 13/09/03.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
